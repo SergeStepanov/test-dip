5fdb7427d35001564bae246163c2216b522f489e
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-13 00:15:20 +0300

    add comp header user page

---
+++ resources/js/Components/HeaderUser.jsx
@@ -0,0 +1,9 @@
+export default function HeaderUser() {
+    return (
+        <header class="page-header">
+            <h1 class="page-header__title">
+                Идём<span>в</span>кино
+            </h1>
+        </header>
+    );
+}
