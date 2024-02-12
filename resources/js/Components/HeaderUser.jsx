11628ee1ad59741dd2279f65f7bb82ee8732fa35
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-13 02:34:22 +0300

    fix class

--- resources/js/Components/HeaderUser.jsx
+++ resources/js/Components/HeaderUser.jsx
@@ -1,7 +1,7 @@
 export default function HeaderUser() {
     return (
-        <header class="page-header">
-            <h1 class="page-header__title">
+        <header className="page-header">
+            <h1 className="page-header__title">
                 Идём<span>в</span>кино
             </h1>
         </header>
