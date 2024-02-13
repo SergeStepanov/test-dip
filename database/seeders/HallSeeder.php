65cffa4b4503f87fb56a4b97ca37efa6aaeb1a00
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-13 23:49:31 +0300

    add seeders

---
+++ database/seeders/HallSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class HallSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('Halls')->insert([
+            'name' => 'Зал 1',
+            'rows' => 6,
+            'cols' => 6,
+            'price_standard' => 300,
+            'price_vip' => 400,
+        ]);
+
+        DB::table('halls')->insert([
+            'name' => 'Зал 2',
+            'rows' => 15,
+            'cols' => 10,
+            'price_standard' => 350,
+            'price_vip' => 450,
+        ]);
+
+        DB::table('halls')->insert([
+            'name' => 'Зал 3',
+            'rows' => 11,
+            'cols' => 9,
+            'price_standard' => 500,
+            'price_vip' => 700,
+        ]);
+    }
+}
---
+++ database/seeders/MovieSeeder.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class MovieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('movies')->insert([
+            'title' => 'Звёздные войны XXIII: Атака клонированных клонов',
+            'duration' => 130,
+            'description' => 'Две сотни лет назад малороссийские хутора разоряла шайка нехристей-ляхов во главе с могущественным колдуном.',
+            'country' => 'США',
+            'poster' => 'poster1.jpg',
+        ]);
+
+        DB::table('movies')->insert([
+            'title' => 'Альфа',
+            'duration' => 96,
+            'description' => '20 тысяч лет назад Земля была холодным и неуютным местом, в котором смерть подстерегала человека на каждом шагу',
+            'country' => 'Франция',
+            'poster' => 'poster2.jpg',
+        ]);
+
+        DB::table('movies')->insert([
+            'title' => 'Хищник',
+            'duration' => 101,
+            'description' => 'Самые опасные хищники Вселенной, прибыв из глубин космоса, высаживаются на улицах маленького городка, чтобы начать свою кровавую охоту. Генетически модернизировав себя с помощью ДНК других видов, охотники стали ещё сильнее, умнее и беспощаднее.',
+            'country' => 'Канада, США',
+            'poster' => 'poster2.jpg',
+        ]);
+ 
+    }
+}
