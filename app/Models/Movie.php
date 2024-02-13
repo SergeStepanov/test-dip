46e45f6981d91dae0e785b964c5144a5ebd1eee2
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-13 20:40:51 +0300

    add model movie -mrc

---
+++ app/Models/Movie.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Movie extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'duration',
+        'description',
+        'country',
+        'poster',
+    ];
+
+    protected $hidden = [
+        'created_up', 'updated_at',
+    ];
+
+    public function sessions(): HasMany
+    {
+        return $this->hasMany(Session::class, 'movie_id');
+    }
+}
