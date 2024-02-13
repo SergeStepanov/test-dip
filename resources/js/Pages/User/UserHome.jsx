7188b6e5d42ea52805b727d5454836cc515bc570
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-13 19:36:42 +0300

    fix class

--- resources/js/Pages/User/UserHome.jsx
+++ resources/js/Pages/User/UserHome.jsx
@@ -153,85 +153,85 @@ export default function UserHome({ auth }) {
                     </div>
                 </section>
 
-                {/* <section class="movie">
-                    <div class="movie__info">
-                        <div class="movie__poster">
+                <section className="movie">
+                    <div className="movie__info">
+                        <div className="movie__poster">
                             <img
-                                class="movie__poster-image"
+                                className="movie__poster-image"
                                 alt="Альфа постер"
                                 src="i/poster2.jpg"
                             />
                         </div>
-                        <div class="movie__description">
-                            <h2 class="movie__title">Альфа</h2>
-                            <p class="movie__synopsis">
+                        <div className="movie__description">
+                            <h2 className="movie__title">Альфа</h2>
+                            <p className="movie__synopsis">
                                 20 тысяч лет назад Земля была холодным и
                                 неуютным местом, в котором смерть подстерегала
                                 человека на каждом шагу.
                             </p>
-                            <p class="movie__data">
-                                <span class="movie__data-duration">
+                            <p className="movie__data">
+                                <span className="movie__data-duration">
                                     96 минут
                                 </span>
-                                <span class="movie__data-origin">Франция</span>
+                                <span className="movie__data-origin">Франция</span>
                             </p>
                         </div>
                     </div>
-                    <div class="movie-seances__hall">
-                        <h3 class="movie-seances__hall-title">Зал 1</h3>
-                        <ul class="movie-seances__list">
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                    <div className="movie-seances__hall">
+                        <h3 className="movie-seances__hall-title">Зал 1</h3>
+                        <ul className="movie-seances__list">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     10:20
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     14:10
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     18:40
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     22:00
                                 </a>
                             </li>
                         </ul>
                     </div>
-                    <div class="movie-seances__hall">
-                        <h3 class="movie-seances__hall-title">Зал 2</h3>
-                        <ul class="movie-seances__list">
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                    <div className="movie-seances__hall">
+                        <h3 className="movie-seances__hall-title">Зал 2</h3>
+                        <ul className="movie-seances__list">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     11:15
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     14:40
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     16:00
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     18:30
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     21:00
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     23:30
                                 </a>
                             </li>
@@ -239,18 +239,18 @@ export default function UserHome({ auth }) {
                     </div>
                 </section>
 
-                <section class="movie">
-                    <div class="movie__info">
-                        <div class="movie__poster">
+                <section className="movie">
+                    <div className="movie__info">
+                        <div className="movie__poster">
                             <img
-                                class="movie__poster-image"
+                                className="movie__poster-image"
                                 alt="Хищник постер"
                                 src="i/poster2.jpg"
                             />
                         </div>
-                        <div class="movie__description">
-                            <h2 class="movie__title">Хищник</h2>
-                            <p class="movie__synopsis">
+                        <div className="movie__description">
+                            <h2 className="movie__title">Хищник</h2>
+                            <p className="movie__synopsis">
                                 Самые опасные хищники Вселенной, прибыв из
                                 глубин космоса, высаживаются на улицах
                                 маленького городка, чтобы начать свою кровавую
@@ -258,77 +258,77 @@ export default function UserHome({ auth }) {
                                 ДНК других видов, охотники стали ещё сильнее,
                                 умнее и беспощаднее.
                             </p>
-                            <p class="movie__data">
-                                <span class="movie__data-duration">
+                            <p className="movie__data">
+                                <span className="movie__data-duration">
                                     101 минута
                                 </span>
-                                <span class="movie__data-origin">
+                                <span className="movie__data-origin">
                                     Канада, США
                                 </span>
                             </p>
                         </div>
                     </div>
-                    <div class="movie-seances__hall">
-                        <h3 class="movie-seances__hall-title">Зал 1</h3>
-                        <ul class="movie-seances__list">
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                    <div className="movie-seances__hall">
+                        <h3 className="movie-seances__hall-title">Зал 1</h3>
+                        <ul className="movie-seances__list">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     09:00
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     10:10
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     12:55
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     14:15
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     14:50
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     16:30
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     18:00
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     18:50
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     19:50
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     20:55
                                 </a>
                             </li>
-                            <li class="movie-seances__time-block">
-                                <a class="movie-seances__time" href="hall.html">
+                            <li className="movie-seances__time-block">
+                                <a className="movie-seances__time" href="hall.html">
                                     22:00
                                 </a>
                             </li>
                         </ul>
                     </div>
-                </section> */}
+                </section>
             </main>
         </GuestLayout>
     );
