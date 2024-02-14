f0ca2904e2ae32cf15abc045351fb9582e14507e
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-14 06:27:08 +0300

    add hendleClosePopupsBtn

--- resources/js/admin/helpFunctions.js
+++ resources/js/admin/helpFunctions.js
@@ -2,3 +2,12 @@ export function hendleToggleHeaderSection({ currentTarget }) {
     currentTarget.classList.toggle("conf-step__header_closed");
     currentTarget.classList.toggle("conf-step__header_opened");
 }
+
+export function hendleClosePopupsBtn(evt) {
+    evt.preventDefault();
+
+    const popup = document.querySelector(".active");
+    if (!popup) return;
+
+    popup.classList.remove("active");
+}
