c4e5c8f22cfc82186a2b07831b45887caf642017
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-14 03:51:56 +0300

    add AdminPageController

---
+++ app/Http/Controllers/AdminPageController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Hall;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+use Inertia\Response;
+
+class AdminPageController extends Controller
+{
+    public function index(): Response
+    {
+        return Inertia::render('Admin/AdminPage', [
+            'halls' => Hall::all(),
+            'movies' => Movie::all(),
+        ]);
+    }
+}
