07572907dc293b0da67110879166bcd20e175c6d
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-15 20:58:34 +0300

    add commit

--- resources/js/admin/helpFunctions.js
+++ resources/js/admin/helpFunctions.js
@@ -1,8 +1,10 @@
+// Скрыть показать контент Section
 export function hendleToggleHeaderSection({ currentTarget }) {
     currentTarget.classList.toggle("conf-step__header_closed");
     currentTarget.classList.toggle("conf-step__header_opened");
 }
 
+// Закрытие модальных окон
 export function hendleClosePopupsBtn(evt) {
     evt.preventDefault();
 
