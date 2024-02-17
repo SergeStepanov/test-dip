4467a77ca18e9bd78155032705ae0671113f7776
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-17 12:44:56 +0300

    commit1

--- resources/js/Pages/Admin/SectionAdminPageContent/OpenSalesContent.jsx
+++ resources/js/Pages/Admin/SectionAdminPageContent/OpenSalesContent.jsx
@@ -3,22 +3,21 @@ import { hendleToggleHeaderSection } from "@/admin/helpFunctions";
 function OpenSalesContent() {
     return (
         <section className="conf-step">
-        <header
-            className="conf-step__header conf-step__header_opened"
-            onClick={(evt) => hendleToggleHeaderSection(evt)}
-        >
-            <h2 className="conf-step__title">Открыть продажи</h2>
-        </header>
-        <div className="conf-step__wrapper text-center">
-            <p className="conf-step__paragraph">
-                Всё готово, теперь можно:
-            </p>
-            <button className="conf-step__button conf-step__button-accent">
-                Открыть продажу билетов
-            </button>
-        </div>
-    </section>
-
+            <header
+                className="conf-step__header conf-step__header_opened"
+                onClick={(evt) => hendleToggleHeaderSection(evt)}
+            >
+                <h2 className="conf-step__title">Открыть продажи</h2>
+            </header>
+            <div className="conf-step__wrapper text-center">
+                <p className="conf-step__paragraph">
+                    Всё готово, теперь можно:
+                </p>
+                <button className="conf-step__button conf-step__button-accent">
+                    Открыть продажу билетов
+                </button>
+            </div>
+        </section>
     );
 }
 
