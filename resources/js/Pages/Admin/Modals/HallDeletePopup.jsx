e62158ce1d3a2488c553ddb58ec639c31d342d95
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-20 00:16:05 +0300

    fix

--- resources/js/Pages/Admin/Modals/HallDeletePopup.jsx
+++ resources/js/Pages/Admin/Modals/HallDeletePopup.jsx
@@ -24,7 +24,7 @@ export default function HallDeletePopup({ delHall }) {
 
         destroy(route("hall.destroy", data));
 
-        hendleClosePopupsBtn(e)
+        hendleClosePopupsBtn(e);
     }
 
     return (
