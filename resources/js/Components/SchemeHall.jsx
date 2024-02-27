7ff832a3987c899ea9978ba00ec77b648812885d
Sergey Stepanov <ser1653285@yandex.ru>
2024-02-27 04:08:43 +0300

    add SchemeHall

---
+++ resources/js/Components/SchemeHall.jsx
@@ -0,0 +1,16 @@
+export default function SchemeHall({ currentHall }) {
+    
+
+    return (
+        <div className="conf-step__row">
+            <span className="conf-step__chair conf-step__chair_standart"></span>
+            <span className="conf-step__chair conf-step__chair_disabled"></span>
+            <span className="conf-step__chair conf-step__chair_standart"></span>
+            <span className="conf-step__chair conf-step__chair_vip"></span>
+            <span className="conf-step__chair conf-step__chair_vip"></span>
+            <span className="conf-step__chair conf-step__chair_standart"></span>
+            <span className="conf-step__chair conf-step__chair_standart"></span>
+            <span className="conf-step__chair conf-step__chair_disabled"></span>
+        </div>
+    );
+}
