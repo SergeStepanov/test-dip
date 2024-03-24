5737f525800bee29397be83043e516c286213feb
Sergey Stepanov <ser1653285@yandex.ru>
2024-03-25 00:27:45 +0300

    фвв Sessionseeder

---
+++ database/seeders/SessionSeeder.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class Sessionseeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('sessions')->insert([
+            'name' => 'Зал 1',
+            'start_time' => '12:00',
+            'hall_id' => 1,
+            'movie_id' => 1,
+        ]);
+
+        DB::table('sessions')->insert([
+            'name' => 'Зал 1',
+            'start_time' => '09:00',
+            'hall_id' => 1,
+            'movie_id' => 1,
+        ]);
+
+        DB::table('sessions')->insert([
+            'name' => 'Зал 1',
+            'start_time' => '17:00',
+            'hall_id' => 1,
+            'movie_id' => 3,
+        ]);
+
+        DB::table('sessions')->insert([
+            'name' => 'Зал 2',
+            'start_time' => '12:00',
+            'hall_id' => 2,
+            'movie_id' => 1,
+        ]);
+
+        DB::table('sessions')->insert([
+            'name' => 'Зал 2',
+            'start_time' => '09:00',
+            'hall_id' => 2,
+            'movie_id' => 3,
+        ]);
+        
+        DB::table('sessions')->insert([
+            'name' => 'Зал 2',
+            'start_time' => '18:00',
+            'hall_id' => 2,
+            'movie_id' => 1,
+        ]);
+
+        DB::table('sessions')->insert([
+            'name' => 'Зал 3',
+            'start_time' => '10:00',
+            'hall_id' => 3,
+            'movie_id' => 2,
+        ]);
+
+        DB::table('sessions')->insert([
+            'name' => 'Зал 3',
+            'start_time' => '14:00',
+            'hall_id' => 3,
+            'movie_id' => 1,
+        ]);
+        
+        DB::table('sessions')->insert([
+            'name' => 'Зал 3',
+            'start_time' => '21:00',
+            'hall_id' => 3,
+            'movie_id' => 2,
+        ]);
+    }
+}
