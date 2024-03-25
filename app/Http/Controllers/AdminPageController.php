4c66e5db3cba5865bc97fc43ab040b6ed0b80b7f
Sergey Stepanov <ser1653285@yandex.ru>
2024-03-26 00:20:08 +0300

    add SessionGridContent setMovieId

--- app/Http/Controllers/AdminPageController.php
+++ app/Http/Controllers/AdminPageController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Hall;
 use App\Models\Movie;
+use App\Models\Session;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
 use Inertia\Response;
@@ -15,6 +16,7 @@ class AdminPageController extends Controller
         return Inertia::render('Admin/AdminPageContent', [
             'halls' => Hall::with('seats')->get(),
             'movies' => Movie::all(),
+            'sessions' => Hall::select('id', 'name')->with('sessions')->get(),
         ]);
     }
 }
