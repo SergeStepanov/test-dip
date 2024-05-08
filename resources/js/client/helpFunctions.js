7cfe85677ffe1f9a548f0151e2eeb1545d4cc336
Sergey Stepanov <ser1653285@yandex.ru>
2024-05-08 19:40:47 +0300

    add addZero date

--- resources/js/client/helpFunctions.js
+++ resources/js/client/helpFunctions.js
@@ -2,3 +2,10 @@ export function dayWeek(day) {
     const strDay = ["Вс", "Пн", "Вт", "Ср", "Чт", "Пт", "Сб"];
     return strDay[day];
 }
+
+export const addZero = (num) => {
+    if (num < 10) {
+        return (num = "0" + num);
+    }
+    return num;
+};
